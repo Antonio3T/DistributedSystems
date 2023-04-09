c15e9cfeb9463af45144ee3ab3f820a493476b7c
António Teixeira <102244893+Antonio3T@users.noreply.github.com>
2023-04-09 01:36:05 +0100

    Add files via upload

---
+++ Server/MClient.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Sockets;
+
+namespace _1
+{
+    public class MClient
+    {
+        private static int count = 1;
+
+        public int Id { get; private set; }
+
+        public string? Name { get; set; }
+
+        public TcpClient Client { get; private set; }
+
+        public string Status { get; set; } = "Offline";
+
+        public MClient(TcpClient client)
+        {
+            if (client == null) throw new ArgumentNullException("client");
+
+            Client = client;
+            Id = count++;
+        }
+    }
+}
