afb4b5ecc5e9e8c5621e83fd583337e3023872de
António Teixeira <102244893+Antonio3T@users.noreply.github.com>
2023-04-09 01:34:12 +0100

    Add files via upload

---
+++ Address.cs
@@ -0,0 +1,28 @@
+﻿namespace _1
+{
+    public class Address
+    {
+        public string Street { get; set; }
+        public string Zip { get; set; }
+        public string DoorNumber { get; set; }
+        public string City { get; set; }
+        public string Municipality { get; set; }
+        public string Ownership { get; set; }
+        public string SendtBy { get; set; }
+
+        public Address(string street, string zip, string doorNumber, string city, string municipality, string ownership, string sentBy)
+        {
+            Street = street;
+            Zip = zip;
+            DoorNumber = doorNumber;
+            City = city;
+            Municipality = municipality;
+            Ownership = ownership;
+            SendtBy = sentBy;
+        }
+        public override string ToString()
+        {
+            return $"{Street}, {Zip}, {DoorNumber}, {City}, {Municipality}, {Ownership}";
+        }
+    }
+}
